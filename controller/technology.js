eb0a5ebc07595da310ed7fcc2d069a43bb4b367a
ziphrax <daverival@hotmail.com>
2016-06-28 01:12:39 +0100

    Updated tech tree and fixed tests

--- controller/technology.js
+++ controller/technology.js
@@ -1,11 +1,11 @@
 var Technology = require('../model/technology');
 
 module.exports = {
-  get : function(req,res){
-      res.json({
-          successful:true,
-          message:"returning players.",
-          docs : Technology
-     });
+  get: function(req, res) {
+    res.json({
+      successful: true,
+      message: "returning players.",
+      docs: Technology
+    });
   }
 }
--- model/technology.js
+++ model/technology.js
@@ -5,29 +5,35 @@ var TechnologyTree = [
         "allows" : [
             {
                 "name" : "Deflectors",
-                "description" : "Guides incoming energy away from the ship. Basic, cheap but affords some protection.", 
+                "description" : "Guides incoming energy away from the ship. Basic, cheap but affords some protection.",
+                "cost" : 1000, 
                 "allows" : [
                     [
                         {
                             "name" : "Improved Deflectors",
                             "description" : "Improvment on the basic deflectors",
+                            "cost" : 2500,
                             "allows" : [
                                 {
                                     "name" : "Shields",
                                     "description" : "Absorbs or nulifies incoming energy. Effective",
+                                    "cost" : 7500,
                                     "allows" : [
                                         {
                                             "name" : "Advanced Shields",
+                                            "cost" : 15000,
                                             "description" : "Improved shields, incredibly effective",
                                             "allows" : []
                                         },
                                         {
                                             "name" : "Frequency Fluctuation",
+                                            "cost" : 11000,
                                             "description" : "Improves the capability of shields vs modulated enegry weapons.",
                                             "allows" : []
                                         },
                                         {
                                             "name" : "Shield Capacitors",
+                                            "cost" : 11000,
                                             "description" : "Improved energy storage copacity, meaning faster shield regeneration times",
                                             "allows" : []
                                         }
@@ -35,6 +41,7 @@ var TechnologyTree = [
                                 },
                                 {
                                     "name" : "Deflector Capacitors",
+                                    "cost" : 7500,
                                     "description" : "Improved energy storage copacity, meaning faster Deflector regeneration times",
                                     "allows" : []
                                 }
@@ -52,17 +59,20 @@ var TechnologyTree = [
             {
                 "name" : "Short Range Lasers",
                 "type" : "Energy",
+                "cost" : 1000,
                 "description" : "Most basic and limited range laser weapons",
                 "allows" : [
                     {
                         "name" : "Long Range Lasers",
                         "type" : "Energy",
+                        "cost" : 5000,
                         "description" : "Improved laser weapons",
                         "allows" : []                
                     },
                     {
                         "name" : "Mining Lasers",
                         "type" : "Energy",
+                        "cost" : 2500,
                         "description" : "Required to gather resources from asteroids",
                         "allows" : []                
                     }
@@ -71,17 +81,20 @@ var TechnologyTree = [
             {
                 "name" : "Mid Range Missles",
                 "type" : "Kinetic",
+                "cost" : 1500,
                 "description" : "Self propelled, chemical based explosive weapons.",
                 "allows" : [
                     {
                         "name" : "Ultra Range Missles",
                         "type" : "Kinetic",
+                        "cost" : 15000,
                         "description" : "Much longer ranged missles",
                         "allows" : []                
                     },
                     {
                         "name" : "Nuclear Missles",
                         "type" : "Nuclear",
+                        "cost" : 25000,
                         "description" : "Most powerfull missles available",
                         "allows" : []                
                     }
@@ -93,15 +106,18 @@ var TechnologyTree = [
         "description" : "Allows for the creation of bigger or faster ships",
         "allows" : [
             {
-                "name" : "Basic Engines",                
+                "name" : "Basic Engines",     
+                "cost" : 1000,           
                 "description" : "The most basic engines, only useful for smaller ships",
                 "allows" : [
                      {
-                        "name" : "Medium Engines",                
+                        "name" : "Medium Engines",     
+                        "cost" : 10000,           
                         "description" : "Can make the most basic ships of the line",
                         "allows" : [
                              {
-                                "name" : "Large Engines",                
+                                "name" : "Large Engines",        
+                                "cost" : 1000000,        
                                 "description" : "Used to move capital class ships",
                                 "allows" : []                
                             }
@@ -112,16 +128,20 @@ var TechnologyTree = [
         ]
     },
     {
-        "name": "Political"
+        "name": "Political",        
+        "cost" : 550000        
     },
     {
-        "name": "Finance"
+        "name": "Finance",        
+        "cost" : 550000
     },
     {
-        "name": "Culture"
+        "name": "Culture",        
+        "cost" : 550000
     },
     {
-        "name": "Special"
+        "name": "Special",        
+        "cost" : 999999
     }
 
 ];
